949a98fa4b219e2dc974f96bb59348113ee906b7
= <=>
2014-07-16 18:08:32 +0530

    Configuring static assets

--- microblog/settings/base.py
+++ microblog/settings/base.py
@@ -95,7 +95,7 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../assets'),
